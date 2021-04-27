e0fd9218d848e14ef9e39ad51ae11a0ae9c355ae
jiyeon <s1990626@ajou.ac.kr>
2021-04-27 17:45:43 +0900

    edit level1/17682

--- programmers/level1/210427_17682.py
+++ programmers/level1/210427_17682.py
@@ -1,86 +1,51 @@
 def solution(dartResult):
     answer = []
-    flag = 'false'
-    for i in range(len(dartResult)):
-        if flag == 'false':
-            if dartResult[i] == 'S' or dartResult[i] == 'D' or dartResult[i] == 'T' or dartResult[i] == '*' or \
-                    dartResult[i] == '#':
-                answer.append(dartResult[i])
-                flag = 'false'
-            else:
-                flag = 'true'
-        elif flag == 'true':
-            if dartResult[i] == 'S' or dartResult[i] == 'D' or dartResult[i] == 'T' or dartResult[i] == '*' or \
-                    dartResult[i] == '#':
-                answer.append(dartResult[i - 1])
-                answer.append(dartResult[i])
-                flag = 'false'
-            else:
-                answer.append(dartResult[i - 1:i + 1])
-                flag = 'false'
-
-    type1 = [0, 0, 0]
-    type2 = [0, 0, 0]
     result = 0
+    new_dart = dartResult.replace("10", "Z")
+
+    for i in range(len(new_dart)):
+        if new_dart[i] == "Z":
+            answer.append("10")
+        else:
+            answer.append(new_dart[i])
+
+    bonus = ["S", "D", "T"]
+    option1 = [0, 0, 0]
+    option2 = [0, 0, 0]
     k = -1
 
     for i in range(len(answer)):
         if i == 2:
             if answer[i] == '*':
-                type1[0] = 1
+                option1[0] = 1
             elif answer[i] == '#':
-                type2[0] = 1
+                option2[0] = 1
         elif i == 4 or i == 5:
             if answer[i] == '*':
-                type1[1] = 1
+                option1[1] = 1
             elif answer[i] == '#':
-                type2[1] = 1
+                option2[1] = 1
         elif i == 6 or i == 7 or i == 8:
             if answer[i] == '*':
-                type1[2] = 1
+                option1[2] = 1
             elif answer[i] == '#':
-                type2[2] = 1
+                option2[2] = 1
 
     for i in range(len(answer)):
         number = 0
-        if answer[i] == "S":
-            k = k + 1
-            number = int(answer[i - 1])
-            if type2[k] == 1:
-                number = number * (-1)
-            for j in range(k, len(type1)):
-                if k == 0:
-                    if j < 2 and type1[j] == 1:
-                        number = number * 2
-                else:
-                    if type1[j] == 1:
-                        number = number * 2
-            result = result + number
-        elif answer[i] == "D":
-            k = k + 1
-            number = int(answer[i - 1]) ** 2
-            if type2[k] == 1:
-                number = number * (-1)
-            for j in range(k, len(type1)):
-                if k == 0:
-                    if j < 2 and type1[j] == 1:
-                        number = number * 2
-                else:
-                    if type1[j] == 1:
-                        number = number * 2
-            result = result + number
-        elif answer[i] == "T":
-            k = k + 1
-            number = int(answer[i - 1]) ** 3
-            if type2[k] == 1:
-                number = number * (-1)
-            for j in range(k, len(type1)):
-                if k == 0:
-                    if j < 2 and type1[j] == 1:
-                        number = number * 2
-                else:
-                    if type1[j] == 1:
-                        number = number * 2
-            result = result + number
+        for n in range(len(bonus)):
+            if answer[i] == bonus[n]:
+                number = int(answer[i - 1]) ** (n + 1)
+                k = k + 1
+                if option2[k] == 1:
+                    number = number * (-1)
+                for j in range(k, len(option1)):
+                    if k == 0:
+                        if j < 2 and option1[j] == 1:
+                            number = number * 2
+                    else:
+                        if option1[j] == 1:
+                            number = number * 2
+                result = result + number
 
     return result
