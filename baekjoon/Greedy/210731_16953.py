bd9a2a06069d78fb018ee81cc724a877782cec3a
jiyeon <s1990626@ajou.ac.kr>
2021-07-31 17:43:07 +0900

    add Greedy/16953

---
+++ baekjoon/Greedy/210731_16953.py
@@ -0,0 +1,20 @@
+#풀이
+A,B=map(int, input().split())
+cnt=1
+while True:
+    if A==B:
+        break
+    elif A>B:
+        cnt=-1
+        break
+    elif B%10==1:
+        B=B//10
+        cnt+=1
+    elif B%2==0:
+        B=B//2
+        cnt+=1
+    else:
+        cnt=-1
+        break
+
+print(cnt)
