5061cfc257e5ae0572cbb83a0d29996da6b33f9c
jiyeon <s1990626@ajou.ac.kr>
2021-08-03 11:41:30 +0900

    solve BFS/7576

--- baekjoon/DFS BFS/210803_7576.py
+++ baekjoon/DFS BFS/210803_7576.py
@@ -1,27 +1,29 @@
-#시간초과
+#deque 사용 -> 시간초과 해결
+from collections import deque
+
 M,N=map(int, input().split())
 arr=[list(map(int, input().split())) for _ in range(N)]
 
-queue=[]
+queue=deque() #deque
 dx=[-1,1,0,0]
 dy=[0,0,-1,1]
 
 for i in range(N):
     for j in range(M):
         if arr[i][j]==1:
-            queue.append((i,j))
+            queue.append((i,j)) #queue에 익은 토마토 위치 넣기
 
 def bfs():
     while queue:
         x=queue[0][0]
         y=queue[0][1]
-        queue.pop(0)
+        queue.popleft()
 
         for i in range(4):
             nx=x+dx[i]
             ny=y+dy[i]
             if nx>=0 and nx<N and ny>=0 and ny<M:
-                if arr[nx][ny]==0:
+                if arr[nx][ny]==0: #아직 방문하지 않은 경우
                     arr[nx][ny]=arr[x][y]+1
                     queue.append((nx,ny))
 
@@ -31,7 +33,7 @@ def result():
         if 0 in num:
             answer = -1
 
-    if answer == -1:
+    if answer == -1: #토마토가 모두 익지는 못하는 상황인 경우
         print(-1)
     else:
         print(max(map(max, arr)) - 1)
