6f65e5c17739cb9f150e2ed6668ffe1db414bfb5
jiyeon <s1990626@ajou.ac.kr>
2021-08-05 23:04:35 +0900

    solve BFS/1697

--- baekjoon/DFS BFS/210805_1697.py
+++ baekjoon/DFS BFS/210805_1697.py
@@ -1,21 +1,23 @@
-#메모리초과
 from collections import deque
 
 N,K=map(int,input().split())
+visited=[0]*100001 #방문검사 확인을 위한 배열
 queue=deque()
 
 def bfs(x):
     queue.append((x,0))
+    visited[x]=1
     while queue:
         x=queue[0][0]
         cnt=queue[0][1]
+        queue.popleft()
         if x==K:
             print(cnt)
             break
-        queue.popleft()
 
-        queue.append((x-1, cnt+1))
-        queue.append((x+1, cnt+1))
-        queue.append((x*2, cnt+1))
+        for nx in (x-1,x+1,x*2):
+            if nx>=0 and nx<=100000 and visited[nx]==0: #범위설정 및 방문검사 -> 메모리초과 해결
+                queue.append((nx,cnt+1))
+                visited[nx]=1
 
 bfs(N)
