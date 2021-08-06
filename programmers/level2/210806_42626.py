1d689523e3043a883323c98cc5666827a4644cc4
jiyeon <s1990626@ajou.ac.kr>
2021-08-06 14:53:08 +0900

    solve level2/42626

--- programmers/level2/210806_42626.py
+++ programmers/level2/210806_42626.py
@@ -1,21 +1,16 @@
-#효율성테스트 실패
-import sys
-sys.setrecursionlimit(10 ** 7)
+#heapq 사용 -> 효율성테스트 통과
+import heapq
 
 def solution(scoville, K):
-    result = 0
+    heapq.heapify(scoville)
     cnt = 0
 
-    while min(scoville) < K:
-        if len(scoville) == 1:  #모든 음식의 스코빌 지수를 k이상으로 만들 수 없는 경우
-            cnt = -1
-            break
+    while scoville[0] < K:
+        if len(scoville) == 1: #모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우
+            return -1
         cnt += 1
-        first = min(scoville)
-        scoville.remove(first)
-        second = min(scoville)
-        scoville.remove(second)
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        heapq.heappush(scoville, first + second * 2)
 
-        result = first + second * 2
-        scoville.append(result)
     return cnt
