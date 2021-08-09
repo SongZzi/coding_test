15e67b6e4bdf238c2a21956f00eaef65d9c79111
jiyeon <s1990626@ajou.ac.kr>
2021-08-09 16:34:56 +0900

    solve level2/42888

--- programmers/level2/210809_42888.py
+++ programmers/level2/210809_42888.py
@@ -1,25 +1,18 @@
-#정확성: 75.0/100.0 (나머지 시간초과)
+#딕셔너리(dictionary) -> 시간초과 해결
 def solution(record):
     answer = []
     arr = []
-    user_id = [] #유저아이디
-    nickname = [] #닉네임
+    user_info = {}  #딕셔너리(dictionary) 선언
 
     for i in range(len(record)):
         arr.append(record[i].split())
-        if arr[i][0] == "Enter":
-            if arr[i][1] not in user_id:
-                user_id.append(arr[i][1])
-                nickname.append(arr[i][2])
-            else:
-                nickname[user_id.index(arr[i][1])] = arr[i][2]
-        elif arr[i][0] == "Change":
-            nickname[user_id.index(arr[i][1])] = arr[i][2]
+        if arr[i][0] == "Enter" or arr[i][0] == "Change":
+            user_info[arr[i][1]] = arr[i][2] #Key=유저아이디, Value=닉네임
 
     for i in range(len(arr)):
         if arr[i][0] == "Enter":
-            answer.append(nickname[user_id.index(arr[i][1])] + "님이 들어왔습니다.")
+            answer.append(user_info[arr[i][1]] + "님이 들어왔습니다.")
         elif arr[i][0] == "Leave":
-            answer.append(nickname[user_id.index(arr[i][1])] + "님이 나갔습니다.")
+            answer.append(user_info[arr[i][1]] + "님이 나갔습니다.")
 
     return answer
