0e26e78de0809603b8a376097dc1e57e1a284eed
jiyeon <s1990626@ajou.ac.kr>
2021-08-11 16:57:35 +0900

    add level2/12951

---
+++ programmers/level2/210811_12951.py
@@ -0,0 +1,17 @@
+def solution(s):
+    answer = ''
+    arr = []
+    s = s.split(' ')
+
+    for i in range(len(s)):
+        arr.append(list(s[i]))
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if j == 0 and arr[i][j].islower() == True:
+                arr[i][j] = arr[i][j].upper()
+            elif j > 0 and arr[i][j].isupper() == True:
+                arr[i][j] = arr[i][j].lower()
+        answer += ''.join(arr[i]) + ' '
+
+    return answer[:-1]
