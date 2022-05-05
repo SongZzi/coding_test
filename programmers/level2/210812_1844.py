88bc2e7f1622dfe780defe875350618be65d3e49
jiyeon <s1990626@ajou.ac.kr>
2022-05-05 23:31:47 +0900

    solve level2/1844 again

--- programmers/level2/210812_1844.py
+++ programmers/level2/210812_1844.py
@@ -1,25 +1,34 @@
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y, maps, visited):
-    queue = [(x, y, 1)]
-    visited[x][y] = 1
-    while queue:
-        x = queue[0][0]
-        y = queue[0][1]
-        z = queue[0][2] #(x,y)에 도착하기 위한 최단 거리
-        if x == len(maps) - 1 and y == len(maps[0]) - 1: #상대팀 진영에 도착한 경우
-            return z
-        queue.pop(0)
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]) and maps[nx][ny] == 1 and visited[nx][ny] == 0:
-                visited[nx][ny] = 1
-                queue.append((nx, ny, z + 1))
-    return -1
+from collections import deque
 
 def solution(maps):
-    visited = [[0] * len(maps[0]) for _ in range(len(maps))]
-    answer = bfs(0, 0, maps, visited) #(0,0)부터 출발
-    return answer
+    n, m = len(maps), len(maps[0])
+    visited = [[0] * m for _ in range(n)]
+
+    dx = [-1, 1, 0, 0]  # 상,하,좌,우
+    dy = [0, 0, -1, 1]  # 상,하,좌,우
+
+    queue = deque()
+
+    # 최단거리 찾기 - bfs
+    def bfs():
+        queue.append((0, 0))
+        visited[0][0] = 1
+        while queue:
+            x = queue[0][0]
+            y = queue[0][1]
+            queue.popleft()
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if nx >= 0 and nx < n and ny >= 0 and ny < m:
+                    if maps[nx][ny] == 1 and visited[nx][ny] == 0:
+                        visited[nx][ny] = 1
+                        maps[nx][ny] = maps[x][y] + 1
+                        queue.append((nx, ny))
+
+    bfs()
+
+    if maps[n - 1][m - 1] == 1:  # 상대 팀 진영에 도착할 수 없는 경우
+        return -1
+    else:
+        return maps[n - 1][m - 1]
